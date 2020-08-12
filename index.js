034af44a5749ec23afb5027af8d53352d00faa09
marumaidana <maruu.mariela@gmail.com>
2020-08-11 21:59:01 -0300

    Clase 13-Heladeria

---
+++ index.js
@@ -0,0 +1,5 @@
+let respuestaDeHelado1 = prompt("Decime un gusto de Helado")
+let respuestaDeHelado2 = prompt("Decime un gusto de Helado")
+let respuestaDeHelado3 = prompt("Decime un gusto de Helado")
+
+alert("Aquí tiene su helado de " + respuestaDeHelado1 + ", " + respuestaDeHelado2 + " y " + respuestaDeHelado3)
